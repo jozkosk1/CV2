6288ab5feca09bf1d2a53a9e88d27d1032c64820
jozkosk1 <jozkosk1@gmail.com>
2016-10-03 15:39:48 +0200

    Uloha1 pomocou knižnice
    nasatvenie portov pomocov štandardnej knižnice

--- src/main.c
+++ src/main.c
@@ -48,12 +48,30 @@ SOFTWARE.
 */
 int main(void)
 {
-  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
-  GPIOA -> MODER |= (1 << 10);		//OUT
-  GPIOA -> OSPEEDR |= (3 << 10);	//Very high speed
-  GPIOA -> PUPDR |= (1 << 10);		//Pull UP
 
-  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+
+	GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_5;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_40MHz;
+
+	GPIO_Init (GPIOA, &GPIO_InitStructure);
+
+
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
+
+	GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_13;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_40MHz;
+
+	GPIO_Init (GPIOC, &GPIO_InitStructure);
+
 
 
 
@@ -76,34 +94,11 @@ int main(void)
   */
 
   /* TODO - Add your application code here */
-  uint32_t p, i = 0;
+
 
   /* Infinite loop */
   while (1)
   {
-	  if (((GPIOC -> IDR) & (1 << 13)) == 0)
-	  {
-		  for (p=0;p<10000;p++)
-			  ;
-		  if (((GPIOC -> IDR) & (1 << 13)) == 0)
-		  {
-			  while (((GPIOC -> IDR) & (1 << 13)) == 0)
-				  ;
-			  for (p=0;p<10000;p++)
-				  ;
-			  i ^=0x01;
-		  }
-	  }
-
-	  if (i == 1)
-	  {
-		  GPIOA -> ODR |= (1 << 5);
-	  }
-	  if (i == 0)
-	  {
-		  GPIOA -> ODR &= ~(1 << 5);
-	  }
-
 
   }
   return 0;
